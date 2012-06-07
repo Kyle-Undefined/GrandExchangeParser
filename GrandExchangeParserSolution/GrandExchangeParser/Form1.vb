129eb35cba8e588282a3302db65098fb6acdd90e
Kyle Undefined <kyle@kyleundefined.com>
2012-06-07 16:36:02 -0400

    New version of the program
    Supports multithreading and doesn't lock up the GUI when running. Code
    updates and improvements as well.

--- GrandExchangeParserSolution/GrandExchangeParser/Form1.vb
+++ GrandExchangeParserSolution/GrandExchangeParser/Form1.vb
@@ -1,9 +1,10 @@
 ﻿Imports System.Net
+Imports System.Threading
 
 Public Class Form1
 
-	Private Sub UpdateStatus(ByVal s As String)
-		Me.Text = "Status: " & s
+	Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+		Me.UpdateStatus("Form loaded")
 	End Sub
 
 	Private Function isMembers(ByVal s As String) As Boolean
@@ -14,21 +15,25 @@ Public Class Form1
 		Return False
 	End Function
 
-	Private Sub btnSearch_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnSearch.Click
+	Private Sub ParseData()
 		Dim ID As String, Name As String, Price As String, Change As String, Members As String
-		Dim client As New WebClient()
 		Dim CurrentPage As Integer = 0, PageCount As Integer = &HFFFF, ItemCount As Integer = 0
 
-		client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)")
-
-		Me.lvItems.Items.Clear()
-		Me.lvItems.BeginUpdate()
-
 		Me.UpdateStatus("Working...")
+		Me.lvItems.BeginUpdate()
 
 		While CurrentPage < PageCount
 			Try
-				Dim Source As String = client.DownloadString(("http://services.runescape.com/m=itemdb_rs/results.ws?page=" & (CurrentPage + 1).ToString() & "&query=") + Me.txtSearch.Text & "&price=all&members=")
+				Dim task As New TaskClass()
+				Dim thread1 As New Threading.Thread(AddressOf task.GetPageData)
+
+				task.argStr = Me.txtSearch.Text
+				task.argInt = CurrentPage
+
+				thread1.Start()
+				thread1.Join()
+
+				Dim Source As String = task.retValStr
 				Dim ItemArray As String() = Source.Split(New String() {"<tr data-item-id="""}, StringSplitOptions.None)
 
 				If PageCount = &HFFFF Then
@@ -42,15 +47,9 @@ Public Class Form1
 					Name = ItemArray(i).Split(New String() {"alt="""}, StringSplitOptions.None)(1).Split(""""c)(0)
 					Price = ItemArray(i).Split(New String() {"<td class=""price"">"}, StringSplitOptions.None)(1).Split(New String() {"</td>"}, StringSplitOptions.None)(0)
 					Change = ItemArray(i).Split(New String() {"<td class=""neutral"">", "<td class=""positive"">", "<td class=""negative"">"}, StringSplitOptions.None)(1).Split(New String() {"</td>"}, StringSplitOptions.None)(0)
-					Members = ItemArray(i).Split(New String() {"title="""}, StringSplitOptions.None)(1).Split(New String() {""">"}, StringSplitOptions.None)(0)
+					Members = Convert.ToString(isMembers(ItemArray(i).Split(New String() {"title="""}, StringSplitOptions.None)(1).Split(New String() {""">"}, StringSplitOptions.None)(0)))
 
-					Members = Convert.ToString(isMembers(Members))
-
-					Me.lvItems.Items.Add(Name)
-					Me.lvItems.Items(lvItems.Items.Count - 1).SubItems.Add(Price)
-					Me.lvItems.Items(lvItems.Items.Count - 1).SubItems.Add(Change)
-					Me.lvItems.Items(lvItems.Items.Count - 1).SubItems.Add(Members)
-					Me.lvItems.Items(lvItems.Items.Count - 1).SubItems.Add(ID)
+					Me.UpdateListView(ID, Name, Price, Change, Members)
 
 					ItemCount += 1
 				Next
@@ -62,29 +61,71 @@ Public Class Form1
 			End Try
 		End While
 
-		For I As Integer = 0 To Me.lvItems.Items.Count - 1
-			Me.lvItems.Items(I).UseItemStyleForSubItems = False
-
-			If I Mod 2 = 0 Then
-				Me.lvItems.Items(I).BackColor = Color.Lavender
-
-				For II As Integer = 1 To 4
-					Me.lvItems.Items(I).SubItems(II).BackColor = Color.Lavender
-				Next
-			End If
-
-			If Me.lvItems.Items(I).SubItems(2).Text.Contains("-") Then
-				Me.lvItems.Items(I).SubItems(2).ForeColor = Color.Red
-			ElseIf Me.lvItems.Items(I).SubItems(2).Text.Contains("+") Then
-				Me.lvItems.Items(I).SubItems(2).ForeColor = Color.Green
-			End If
-		Next
-
-		Me.lvItems.EndUpdate()
 		Me.UpdateStatus("Found " & ItemCount.ToString() & " item(s) across " & PageCount.ToString() & " page(s)")
+		Me.lvItems.EndUpdate()
 	End Sub
 
-	Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-		Me.UpdateStatus("Form loaded")
+	Private Sub btnSearch_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnSearch.Click
+		Dim thread1 As New Threading.Thread(AddressOf Me.ParseData)
+
+		Me.lvItems.Items.Clear()
+		thread1.Start()
+	End Sub
+
+	Public Delegate Sub DelegateUpdateStatus(ByVal s As String)
+	Private Sub UpdateStatus(ByVal s As String)
+		If Me.InvokeRequired Then
+			Dim d As New DelegateUpdateStatus(AddressOf UpdateStatus)
+			Me.Invoke(d, New Object() {s})
+		Else
+			Me.Text = "Status: " & s
+		End If
+	End Sub
+
+	Public Delegate Sub DelegateUpdateListView(ByVal ID As String, ByVal Name As String, ByVal Price As String, ByVal Change As String, ByVal Members As String)
+	Private Sub UpdateListView(ByVal ID As String, ByVal Name As String, ByVal Price As String, ByVal Change As String, ByVal Members As String)
+		If Me.InvokeRequired Then
+			Dim d As New DelegateUpdateListView(AddressOf UpdateListView)
+			Me.Invoke(d, New Object() {ID, Name, Price, Change, Members})
+		Else
+			Me.lvItems.Items.Add(Name)
+			Me.lvItems.Items(lvItems.Items.Count - 1).SubItems.Add(Price)
+			Me.lvItems.Items(lvItems.Items.Count - 1).SubItems.Add(Change)
+			Me.lvItems.Items(lvItems.Items.Count - 1).SubItems.Add(Members)
+			Me.lvItems.Items(lvItems.Items.Count - 1).SubItems.Add(ID)
+
+			For I As Integer = 0 To Me.lvItems.Items.Count - 1
+				Me.lvItems.Items(I).UseItemStyleForSubItems = False
+
+				If I Mod 2 = 0 Then
+					Me.lvItems.Items(I).BackColor = Color.Lavender
+
+					For II As Integer = 1 To 4
+						Me.lvItems.Items(I).SubItems(II).BackColor = Color.Lavender
+					Next
+				End If
+
+				If Me.lvItems.Items(I).SubItems(2).Text.Contains("-") Then
+					Me.lvItems.Items(I).SubItems(2).ForeColor = Color.Red
+				ElseIf Me.lvItems.Items(I).SubItems(2).Text.Contains("+") Then
+					Me.lvItems.Items(I).SubItems(2).ForeColor = Color.Green
+				End If
+			Next
+		End If
+	End Sub
+End Class
+
+Class TaskClass
+	Friend argInt As Integer
+	Friend argStr As String
+	Friend retValStr As String
+
+	Sub GetPageData()
+		Dim client As New WebClient()
+
+		client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6")
+		Me.retValStr = client.DownloadString(("http://services.runescape.com/m=itemdb_rs/results.ws?page=" & (Me.argInt + 1).ToString() & "&query=") + Me.argStr & "&price=all&members=")
+
+		client.Dispose()
 	End Sub
 End Class
